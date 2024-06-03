a5eaba34b12a0a06c2f2e790d30df2b6293ecd61
Arunava Kirtan <kirtanarunava145@gmail.com>
2024-06-03 19:19:47 +0530

    optimize image

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,12 +1,13 @@
 import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
 import PlatFormIcons from "./PlatFormIcons";
 import Score from "./Score";
+import { getOptimizeUrl } from "../services/optimizeImage";
 
 const GameCard = ({ game }) => {
   return (
     <Card borderRadius="7px" overflow="hidden">
       <Image
-        src={game.background_image}
+        src={getOptimizeUrl(game.background_image)}
         height="75%"
         objectFit={"cover"}
       ></Image>
