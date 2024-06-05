b13cd0d4254716365b749d29dde477b1aa92500f
Arunava Kirtan <kirtanarunava145@gmail.com>
2024-06-05 11:34:28 +0530

    refactor game grid

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -18,23 +18,22 @@ const GameGrid = ({
   );
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
-        spacing={3}
-        padding={5}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <CardSkeleton key={skeleton}></CardSkeleton>
-          ))}
-        {games.map((game) => (
-          <GameCard game={game} key={game}></GameCard>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
+      spacing={3}
+      padding={5}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <CardSkeleton key={skeleton}></CardSkeleton>
         ))}
-      </SimpleGrid>
-    </>
+      {games.map((game) => (
+        <GameCard game={game} key={game}></GameCard>
+      ))}
+    </SimpleGrid>
   );
 };
 
