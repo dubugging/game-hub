c99ab7b985a9b495557ad8abd8e194473a889dcf
dubugging <kirtanarunava@gmail.com>
2024-07-26 14:00:41 +0530

    configure quer client

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import theme from "./theme.js";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
       <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
